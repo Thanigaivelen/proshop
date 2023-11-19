fa135ba5b75fcfdcce7544c86aa9302b5e6e9ab1
Thanigaivelen <thanigaivelen2002@gmail.com>
2023-11-20 02:31:40 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config();
+import connectDB from './config/db.js';
+import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 
-import products from './data/products.js';
+
+import productRoutes from './routes/productRoutes.js'
 
 const port=process.env.PORT || 5000;
+connectDB();
 
 
 
@@ -16,13 +20,10 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send('Api is running!!!!')
 })
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
-app.get('/api/products/:id',(req,res)=>{
-    const product=products.find((p)=>p._id===req.params.id)
-    res.json(product)
-})
+app.use('/api/products',productRoutes)
+app.use(notFound)
+app.use(errorHandler)
+
 
 
 
